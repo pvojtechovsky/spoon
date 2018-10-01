99cb000b8125fb35e36431566c948a33f47b87d4
Pavel Vojtechovsky <p.vojtechovsky@post.cz>
2018-10-01 23:06:31 +0200

    fix(sniper): it is OK that muted state is changed

--- src/main/java/spoon/support/sniper/SniperJavaPrettyPrinter.java
+++ src/main/java/spoon/support/sniper/SniperJavaPrettyPrinter.java
@@ -382,14 +382,6 @@ public class SniperJavaPrettyPrinter extends DefaultJavaPrettyPrinter {
 			mutableTokenWriter.setMuted(muted);
 			code.run();
 		} finally {
-			//assure that muted status did not changed in between
-			if (mutableTokenWriter.isMuted() != muted) {
-				if (mutableTokenWriter.isMuted()) {
-					throw new SpoonException("Unexpected state: Token writer is muted after scanning");
-				} else {
-					throw new SpoonException("Unexpected state: Token writer is not muted after scanning");
-				}
-			}
 			mutableTokenWriter.setMuted(originMuted);
 		}
 	}
